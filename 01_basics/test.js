3888995682ecbf670a54fd9c689b621614370550
Shivam Sharma <140006772+sharma20shivam02@users.noreply.github.com>
2024-09-24 12:09:42 +0000

    variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
 console.log("Hey")
+
+
+// Variables:
+const empId = 123
+let empMail = "abc@ab.com"
+var empPass = "123123"
+empLocation = "delhi"   // can declare variables without any specific data handler
+
+console.table([empId,empMail, empPass, empLocation])
