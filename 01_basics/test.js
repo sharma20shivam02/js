2ff381d8aef5ad6db8b209e62a30eccdc659dca0
Shivam Sharma <140006772+sharma20shivam02@users.noreply.github.com>
2024-09-24 12:25:39 +0000

    conversion

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -8,3 +8,34 @@ var empPass = "123123"
 empLocation = "delhi"   // can declare variables without any specific data handler
 
 console.table([empId,empMail, empPass, empLocation])
+
+
+
+
+// data types:
+/*
+    number 
+    bigint
+    string
+    boolean
+    null
+    undefined
+    symbols => unique
+
+    //object
+*/
+
+
+console.log(typeof undefined);      // undefined
+console.log(typeof null);           // object
+
+
+
+let sc = "33abc"
+console.log(typeof sc)
+let val = Number(sc)
+
+console.log(typeof val);        //Number
+console.log(val)                //NaN -- Not a Number
+
+
